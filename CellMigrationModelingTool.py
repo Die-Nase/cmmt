29071aee57e58537da6957fc30dcd356be1abd5e
Die-Nase <l.lamparter@gmail.com>
2021-04-20 10:30:26 +0200

    first commit of empty file

---
+++ CellMigrationModelingTool.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class LevyFlight:
+    def __init__(self, repeats):
+        self.repeats = repeats
+        self.tracks = list()
+    def plotDistribution():
+        
+LF = LevyFlight(10)
+print(LF.repeats)
+print(LF.tracks)
+        
