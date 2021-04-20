6503676ba1f6b535576953cdce7f6c0be1ae725f
Die-Nase <59151217+Die-Nase@users.noreply.github.com>
2021-04-21 01:42:58 +0200

    expanded class point by setter and getter

--- CellMigrationModelingTool.py
+++ CellMigrationModelingTool.py
@@ -4,14 +4,98 @@ import pandas as pd
 from scipy.stats import levy
 
 class point:
-    def __init__(self, x = 0, y = 0, z = 0):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, xyz = (None, None, None), spherical = (None, None, None), t = None):
+        self.__xyz = xyz
+        self.__spherical = spherical
     
-    def polar(self):
-        pass
+    @property
+    def x(self):
+        return self.__xyz[0]
+    
+    @property
+    def y(self):
+        return self.__xyz[1]
+    
+    @property
+    def z(self):
+        return self.__xyz[2]
+    
+    @property
+    def xyz(self):
+        return self.__xyz
+    
+    @property
+    def mag(self):
+        return self.__spherical[0]
+    
+    @property
+    def polar_angle(self):
+        return self.__spherical[1]
+    
+    @property
+    def azimuthal_angle(self):
+        return self.__spherical[2]
+    
+    @property
+    def spherical(self):
+        return self.__spherical
+    
+    @x.setter
+    def x(self, x):
+        xyz = np.array(self.__xyz)
+        xyz[xyz==None] = 0
+        xyz[0] = x
+        self.__xyz = tuple(xyz)
+        self.__shperical = spherical(self.__xyz)
+        
+    @y.setter
+    def y(self, y):
+        xyz = np.array(self.__xyz)
+        xyz[xyz==None] = 0
+        xyz[1] = y
+        self.__xyz = tuple(xyz)
+        self.__shperical = spherical(self.__xyz)
+        
+    @z.setter
+    def x(self, z):
+        xyz = np.array(self.__xyz)
+        xyz[xyz==None] = 0
+        xyz[2] = z
+        self.__xyz = tuple(xyz)
+        self.__shperical = spherical(self.__xyz)
+    
+    @mag.setter
+    def mag(self, mag):
+        
+        
+    @spherical.setter
+    def spherical(self, xyz):
+        x, y, z = xyz[0], xyz[1], xyz[2]
+        magnitude = np.sqrt(x**2 + y**2 + z**2)
+        if magnitude == 0:
+            polar_angle = None
+            azimuthal_angle = None
+        elif x == 0:
+            azimuthal_angle = None
+            polar_angle = np.arccos(z/magnitude)
+        else:
+            polar_angle = np.arccos(z/magnitude)
+            azimuthal_angle = np.arctan(y/x)
+        self.__spherical = (magnitude, polar_angle, azimuthal_angle)
+    
+    @xyz.setter
+    def xyz(self, spherical):
+        magnitude = spherical[0]
+        polar_angle = spherical[1]
+        azimuthal_angle = spherical[2]
+        x = magnitude * np.sin(polar_angle) * np.cos(azimuthal_angle)
+        y = magnitude * np.sin(polar_angle) * np.sin(azimuthal_angle)
+        z = magnitude * np.cos(polar_angle)
+        self.__xyz = (x, y, z)
+        return self.__xyz
+            
 
+            
 class vector:
     def __init__(self, mag, polarangle, azimuthalangle, start, end):
 
